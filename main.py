008b6ed01913eb3835a10255b35db57983ed47ad
MiguelMancha <maikymancha05@gmail.com>
2026-06-19 10:49:27 -0600

    clean: eliminar archivos duplicados ci.yml y main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-def main():
-    print("=========================================")
-    print("¡Aplicación ejecutada con éxito!")
-    print("Validación de contenedor Docker: PASADA")
-    print("=========================================")
-
-if __name__ == "__main__":
-    main()
